84a5d1ff709b5bbe0dcc11fb38a2638d14324dd5
Brian Pickens <3659859+brian-pickens@users.noreply.github.com>
2023-05-07 18:38:32 +0000

    Set exit code if failed tests detected adds debug output for actions

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -4,8 +4,25 @@ echo -e "---------------\nRunning Action\n---------------\n"
 
 cd /github/workspace
 gotestsum --junitfile TEST-RESULTS.xml --jsonfile TEST-RESULTS.json $modules
-echo "junit-report=$(cat TEST-RESULTS.xml)" >> $GITHUB_OUTPUT
-echo "json-report=$(cat TEST-RESULTS.json)" >> $GITHUB_OUTPUT
-echo "markdown-report=$(junit2md TEST-RESULTS.xml)" >> $GITHUB_OUTPUT
+junit_report=$(cat TEST-RESULTS.xml)
+json_report=$(cat TEST-RESULTS.json)
+markdown_report=$(junit2md TEST-RESULTS.xml)
+
+echo "junit-report=${junit_report}" >> $GITHUB_OUTPUT
+echo "json-report=${json_report}" >> $GITHUB_OUTPUT
+echo "markdown-report=${markdown_report}" >> $GITHUB_OUTPUT
+
+echo "::debug::junit-report:${junit_report}"
+echo "::debug::json-report:${json_report}"
+echo "::debug::jmarkdown-report:${markdown_report}"
+
+failed_tests=$(jq 'select(.Action | contains("fail"))' << $json_report)
+echo "::debug::failed_tests:${failed_tests}"
 
 echo -e "\n---------------\nAction complete\n---------------"
+
+if [ -z ${failed_tests} ]; then
+    exit 1;
+fi;
+
+exit 0;
