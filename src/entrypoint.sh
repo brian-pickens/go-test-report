aa197cae4a8ae0e3217812d8f9e0cd267383fddb
Brian Pickens <3659859+brian-pickens@users.noreply.github.com>
2023-05-08 03:02:49 +0000

    test debug

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -13,9 +13,10 @@ echo "junit-report: ${junit_report}" >> $GITHUB_OUTPUT
 echo "json-report: ${json_report}" >> $GITHUB_OUTPUT
 echo "markdown-report<<EOF"$'\n'"$markdown_report"$'\n'EOF >> $GITHUB_ENV
 
-echo "::debug::junit-report=${junit_report}"
-echo "::debug::json-report=${json_report}"
-echo "::debug::markdown-report=${markdown_report}"
+echo "::debug::test"
+# echo "::debug::junit-report=${junit_report}"
+# echo "::debug::json-report=${json_report}"
+# echo "::debug::markdown-report=${markdown_report}"
 
 failed_tests=$(jq 'select(.Action | contains("fail"))' << $json_report)
 echo "::debug::failed_tests:${failed_tests}"
