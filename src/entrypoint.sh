b875f62180f4ed7abf7152d332c51f2c7347749f
Brian Pickens <3659859+brian-pickens@users.noreply.github.com>
2023-05-08 04:28:25 +0000

    Fix if statements

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -20,7 +20,7 @@ echo "junit-report<<EOF"$'\n'"${junit_report}"$'\n'EOF >> $GITHUB_OUTPUT
 echo "markdown-report<<EOF"$'\n'"${markdown_report}"$'\n'EOF >> $GITHUB_OUTPUT
 
 # Set Summary
-if [ $INPUT_SUMMARY == "true"]; then
+if [ "$INPUT_SUMMARY" = "true"]; then
     echo "${markdown_report}" >> $GITHUB_STEP_SUMMARY
 fi;
 
@@ -48,7 +48,7 @@ echo "::debug::failed_tests:${failed_tests}"
 echo -e "\n---------------\nAction complete\n---------------"
 
 # Exit successfully if report-only mode
-if [ $INPUT_REPORT_ONLY ]; then
+if [ "$INPUT_REPORT_ONLY" = "true" ]; then
     exit 0;
 fi;
 
--- test/test-entrypoint.sh
+++ test/test-entrypoint.sh
@@ -3,9 +3,10 @@
 # Simulate setting input environment variables
 export INPUT_PACKAGES="./..."
 export INPUT_SUMMARY="true"
-export INPUT_REPORT_ONLY="false"
+export INPUT_REPORT_ONLY="true"
 export INPUT_GO111MODULE="auto"
 export GITHUB_OUTPUT="/github_output.txt"
+export GITHUB_STEP_SUMMARY="/github_step_summary.txt"
 
 # Run action entrypoint
 /entrypoint.sh
