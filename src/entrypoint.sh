f72e0f48aa1a55242c7bec9543b8288d314eca26
Brian Pickens <3659859+brian-pickens@users.noreply.github.com>
2023-05-08 02:47:42 +0000

    Use the correct variables for inputs

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,8 +3,8 @@
 echo -e "---------------\nRunning Action\n---------------\n"
 
 cd /github/workspace
-go env -w GO111MODULE="$2"
-gotestsum --junitfile TEST-RESULTS.xml --jsonfile TEST-RESULTS.json $1
+go env -w GO111MODULE="$INPUT_GO111MODULE"
+gotestsum --junitfile TEST-RESULTS.xml --jsonfile TEST-RESULTS.json $INPUT_PACKAGES
 junit_report=$(cat TEST-RESULTS.xml)
 json_report=$(jq -s '.' TEST-RESULTS.json)
 markdown_report=$(junit2md TEST-RESULTS.xml)
--- test/test-entrypoint.sh
+++ test/test-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh -l
 
 # Simulate setting input environment variables
-export modules="./..."
+export INPUT_PACKAGES="./..."
+export INPUT_GO111MODULE="auto"
 export GITHUB_OUTPUT="/github_output.txt"
 
 # Run action entrypoint
